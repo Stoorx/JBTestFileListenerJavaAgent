286d3056e316c951f6287e5d8cae60231c00a31d
Stoorx <me@stoorx.one>
2023-01-18 21:49:23 +0300

    Update StubHost.java

--- src/test/java/one/stoorx/listenerAgent/StubHost.java
+++ src/test/java/one/stoorx/listenerAgent/StubHost.java
@@ -1,11 +1,18 @@
 package one.stoorx.listenerAgent;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class StubHost {
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void main(String[] args) throws IOException {
         Files.readString(Paths.get("a.txt"));
+        Files.newInputStream(Paths.get("a.txt"));
+        Files.newByteChannel(Paths.get("a.txt"));
+        new FileReader("a.txt").read();
+        new RandomAccessFile("a.txt", "r").read();
     }
 }
